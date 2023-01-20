aaa038e8a54eacb53f74b7737638afca2756fca3
Vadim Rusakov <rusakov.vadim.94@gmail.com>
2023-01-20 12:41:45 +0300

    add_validator_for_check_strategy_len_data

--- main/app/schemas/strategy.py
+++ main/app/schemas/strategy.py
@@ -1,8 +1,14 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, root_validator
 
 
 class StrategyData(BaseModel):
     strategy_id: int
     data: List[int]
+
+    @root_validator(pre=True)
+    def check_len_data_for_second_strategy(cls, values):
+        if values["strategy_id"] == 2 and len(values["data"]) > 10:
+            raise ValueError(f"Strategy 2 cannot accept more than 10 values")
+        return values
